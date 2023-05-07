34dfa6bb15af8703c0dc2c983e24fee632e88c1d
Maryna Biesiedina <43865696+M23Fox@users.noreply.github.com>
2023-05-07 17:38:31 +0100

    7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const itemsBox = document.querySelector('.gallery');
+const galleryBox = galleryItems
+    .map(({ preview, original, description }) =>         `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"/>
+          </a>
+        </li>`)
+      .join('');  
+    
+itemsBox.insertAdjacentHTML('beforeend', galleryBox);
 
+// itemsBox.addEventListener('click', onImageClick);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 console.log(galleryItems);
